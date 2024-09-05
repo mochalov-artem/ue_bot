059f22fff62b066b95aba4ed32ef650ac1315f27
artem <artemmochalov220@gmail.com>
2024-09-06 01:57:00 +0400

    handler   cmd_progressbar

--- handlers/commands/db_statistics.py
+++ handlers/commands/db_statistics.py
@@ -1,7 +1,8 @@
 from aiogram import Router, F
 from aiogram.filters import Command
 from aiogram.types import Message
-from aiosqlite import Connection
+from sqlalchemy import text
+from sqlalchemy.ext.asyncio import AsyncSession
 
 from utils.progressbar import progressbar
 
@@ -9,9 +10,8 @@ router = Router()
 
 
 @router.message(F.text, Command("stats"))
-async def progress_bar(message: Message, async_con: Connection):
-    cursor = await async_con.execute("SELECT COUNT(DISTINCT(pic_id)) FROM pic_user_rate")
-    res = await cursor.fetchone()
-    print(res)
+async def cmd_progress_bar(message: Message, session: AsyncSession):
+    cursor = await session.execute(text("SELECT COUNT(DISTINCT(pic_id)) FROM pic_user_rate"))
+    res = cursor.fetchone()
     current = res[0]
     await message.answer(text=progressbar(current, total=85_015, bar_length=15))
