34ea74036e41fcf08d257780bf5a4dd90214f03e
artem <artemmochalov220@gmail.com>
2024-09-13 11:25:55 +0400

    handlers   sched_mem     + try except

--- handlers/commands/scheduled_mem.py
+++ handlers/commands/scheduled_mem.py
@@ -24,31 +24,34 @@ async def cmd_sched(message: Message, session: AsyncSession, bot: Bot, mega: Meg
     if not FLAG:
         FLAG = 1
         while True:
-            db_query = await session.execute(
-                text("""SELECT * FROM images WHERE file_used=0 ORDER BY RANDOM() LIMIT 1;""")
-            )
-            image = db_query.fetchone()
+            try:
+                db_query = await session.execute(
+                    text("""SELECT * FROM images WHERE file_used=0 ORDER BY RANDOM() LIMIT 1;""")
+                )
+                image = db_query.fetchone()
 
-            if not image:
-                return await bot.send_message(chat_id=CHAT_ID, text='Все фото были просмотрены, поздравляю!')
+                if not image:
+                    return await bot.send_message(chat_id=CHAT_ID, text='Все фото были просмотрены, поздравляю!')
 
-            f_id = image[1]
-            file_key = tuple(map(int, image[2].split(',')))
-            bytes_photo = await (mega_func_2(f_id, file_key, mega))
-            photo_id = await bot.send_photo(
-                chat_id=CHAT_ID,
-                photo=BufferedInputFile(bytes_photo, f"{image[3]}.jpg"),
-                reply_markup=get_rate_keyboard(pic_id=image[1]),
-            )
-            file_tg_id = photo_id.photo[-1].file_id
-            values = {
-                'file_used': 1,
-                'file_tg_id': file_tg_id,
-                'id': image[0],
-            }
-            await session.execute(
-                text(f"""UPDATE images SET file_used=:file_used, file_tg_id=:file_tg_id WHERE id=:id;"""),
-                values
-            )
-            await session.commit()
-            await sleep(TIMER)
+                f_id = image[1]
+                file_key = tuple(map(int, image[2].split(',')))
+                bytes_photo = await (mega_func_2(f_id, file_key, mega))
+                photo_id = await bot.send_photo(
+                    chat_id=CHAT_ID,
+                    photo=BufferedInputFile(bytes_photo, f"{image[3]}.jpg"),
+                    reply_markup=get_rate_keyboard(pic_id=image[1]),
+                )
+                file_tg_id = photo_id.photo[-1].file_id
+                values = {
+                    'file_used': 1,
+                    'file_tg_id': file_tg_id,
+                    'id': image[0],
+                }
+                await session.execute(
+                    text(f"""UPDATE images SET file_used=:file_used, file_tg_id=:file_tg_id WHERE id=:id;"""),
+                    values
+                )
+                await session.commit()
+                await sleep(TIMER)
+            except Exception as e:
+                await bot.send_message(chat_id=CHAT_ID, text="АШЫПКА хехеее))")
